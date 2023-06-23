436abaf878dd2c93bbdc8b7d9e7dafed530e37c5
Luu Vu <vu7178@gmail.com>
2023-06-24 00:48:39 +0700

    ADD

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ app.engine(
 );
 
 app.set('view engine', 'hbs');
-app.set('   views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
