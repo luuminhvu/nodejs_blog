78090828eec170c7998b82613cbe6687995fcd19
Luu Vu <vu7178@gmail.com>
2023-06-23 22:24:55 +0700

    routes

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,9 @@
+class NewsController{
+    index(req, res){
+        res.render('news');
+    }
+    show(req, res){
+        res.send('News detail');
+    }
+}
+module.exports = new NewsController; // xuất ra ngoài để sử dụng
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded(
+    {extended: true}
+));
+app.use(express.json());
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -15,17 +20,5 @@ app.engine('hbs', handlebars.create({
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
+route(app);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
